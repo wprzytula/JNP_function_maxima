df776f961f8300fe12b09cd30e88370b40d039c9
wprzytula <wp418383@students.mimuw.edu.pl>
2020-12-29 18:27:30 +0100

    Enhanced prototype

--- function_maxima.h
+++ function_maxima.h
@@ -25,12 +25,58 @@
 
 
 
+#include <utility>
+#include <set>
+
 template<typename A, typename V>
 class FunctionMaxima {
-    // Konstruktor bezparametrowy (tworzy funkcję o pustej  dziedzinie),
+public:
+    // Nie powinno być możliwe bezpośrednie konstruowanie obiektów typu
+    // point_type, ale zezwalamy na ich kopiowanie i przypisywanie.
+    class point_type {
+    private:
+        A _arg;
+        V _value;
+        explicit point_type(A arg, V value) : _arg(arg), _value(value) {}
+    public:
+        point_type(const point_type&) = default;
+        point_type(point_type&&) noexcept = default;
+        point_type& operator= (const point_type&) = default;
+        point_type& operator= (point_type&&) noexcept = default;
+        // Zwraca argument funkcji.
+        A const& arg() const {
+            return _arg;
+        }
+        // Zwraca wartość funkcji w tym punkcie.
+        V const& value() const {
+            return _value;
+        }
+        // Zadaje porządek liniowy na point_type identyczny z porządkiem na argumentach.
+        bool operator< (point_type const& other) {
+            return _arg < other._arg;
+        }
+    };
+
+    using function_set = std::set<point_type, std::less<point_type>>;
+
+    struct maxima_order {
+        bool operator() (const point_type& x, const point_type& y) const {
+            return x.value() > y.value() || (!(y.value() < x.value()) && x.arg() < y.arg());
+        }
+    };
+
+    using maxima_set = std::set<point_type, maxima_order>;
+
+    class InvalidArg : std::exception {};
+
+    // Konstruktor bezparametrowy (tworzy funkcję o pustej dziedzinie),
     //  konstruktor kopiujący i operator=. Dwa ostatnie powinny mieć
     //  sensowne działanie.
     FunctionMaxima() = default;
+    FunctionMaxima(const FunctionMaxima&) = default;
+    FunctionMaxima(FunctionMaxima&&) noexcept = default;
+    FunctionMaxima& operator=(const FunctionMaxima&) = default;
+    FunctionMaxima& operator=(FunctionMaxima&&) noexcept = default;
 
     // Zwraca wartość w punkcie a, rzuca wyjątek InvalidArg, jeśli a nie
     // należy do dziedziny funkcji. Złożoność najwyżej O(log n).
@@ -50,58 +96,43 @@ class FunctionMaxima {
 
     }
 
-
-    using point_type = class {
-        // Zwraca argument funkcji.
-        A const& arg() const {
-
-        }
-        // Zwraca wartość funkcji w tym punkcie.
-        V const& value() const;
-    };
-//    Nie powinno być możliwe bezpośrednie konstruowanie obiektów typu
-//    point_type, ale zezwalamy na ich kopiowanie i przypisywanie.
-
 //    Typ iterator zachowujący się tak jak bidirectional_iterator
 //    (http://www.cplusplus.com/reference/iterator/BidirectionalIterator),
 //            iterujący po punktach funkcji.
 //    Dla zmiennej it typu wyrażenie *it powinno być typu point_type const&.
-    using iterator = class {
+    using iterator = typename function_set::const_iterator;
 
-    };
 //    Metody dające dostęp do punktów funkcji:
     // iterator wskazujący na pierwszy punkt
-    iterator begin() const {
-
+    iterator begin() const noexcept {
+        return fun.begin();
     }
 
     // iterator wskazujący za ostatni punkt
-    iterator end() const {
-
+    iterator end() const noexcept {
+        return fun.end();
     }
 
     // Iterator, który wskazuje na punkt funkcji o argumencie a lub end(),
     // jeśli takiego argumentu nie ma w dziedzinie funkcji.
-    iterator find(A const& a) const {
-
+    iterator find(A const& a) const noexcept {
+        return fun.find(a);
     }
 
     // Typ mx_iterator zachowujący się znów jak bidirectional_iterator,
     // iterujący po lokalnych maksimach funkcji.
     // Dla zmiennej it typu wyrażenie *it powinno być typu point_type const&.
 
-    using mx_iterator = class {
-
-    };
+    using mx_iterator = typename maxima_set::const_iterator;
 
     // iterator wskazujący na pierwsze lokalne maksimum
-    mx_iterator mx_begin() const {
-
+    mx_iterator mx_begin() const noexcept {
+        return maxima.begin();
     }
 
     // iterator wskazujący za ostatnie lokalne maksimum
-    mx_iterator mx_end() const {
-
+    mx_iterator mx_end() const noexcept {
+        return maxima.end();
     }
 
 //    Jeśli przez k oznaczymy rozmiar zbioru lokalnych maksimów, to powyższe
@@ -116,16 +147,14 @@ class FunctionMaxima {
 
 //    Typ size_type reprezentujący rozmiar dziedziny i funkcja zwracająca ten
 //            rozmiar:
-    using size_type = class {
+    using size_type = typename function_set::size_type;
 
-    };
-
-      size_type size() const {
-
-      }
-
-
-
-
+    size_type size() const noexcept {
+        return fun.size();
+    }
+private:
+    function_set fun; // proponuję tu trzymać elementy stosując komparator argumentu
+    maxima_set maxima; // tu zaś stosujemy komparator odwrotnie leksykograficzny,
+                       // począwszy od drugiej współrzędnej
 };
 #endif //MAKSIMA_FUNCTION_MAXIMA_H
